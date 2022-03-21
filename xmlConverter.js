3dca60f842931ebc2224435880e6aefb873fc3cb
Alfredo Sugawara <nicora221@gmail.com>
2022-03-21 14:46:16 -0400

    Chore(Using xml2json instead of xml2js)

--- xmlConverter.js
+++ xmlConverter.js
@@ -1,18 +1,13 @@
-import xml2js from 'xml2js';
+import parser from 'xml2json';
 import JSONToCSV from 'json2csv';
-import xmlFile from "./Facility.js"
 import FileSystem from "fs"
 
 
-export default function converter(data,fileName){
-    let parser = new xml2js.Parser({
-        explicitArray:false,
-    })
-    parser.parseString(data, function (err, result){//Parses xml string to JSON
-        if(err) throw new Error(err)
-
-        const parsed = result.Table_Facility.T_Facility //keying into proper level of JSON object
-        let mapped = parsed.map(data =>{ //returning array of formated objects with appropriate mapping
+function converter(fileName,newFileName){
+    FileSystem.readFile( `./${fileName}.xml`, function(err, data) {
+        let json = parser.toJson(data);
+        let values = (JSON.parse(json).Table_Facility.T_Facility);
+        let mapped = values.map(data =>{ //returning array of formated objects with appropriate mapping
             return {
                 property_id:data.Facility_ID || "", //added the or condition to return an empty string rather than "undefined"
                 account_number:data.Facility_Account_Number || "",
@@ -25,22 +20,21 @@ export default function converter(data,fileName){
             }
         })
         const csv = JSONToCSV.parse(mapped,//intaking mapped array of JSON objects and formating them into csv
-            {fields: [
-                "property_id",
-                "account_number",
-                "name",
-                "address1",
-                "city",
-                "state_prov",
-                "postal_code",
-                "primary_contact_id"
-            ]
-            })
-            console.log(csv)
-        FileSystem.writeFileSync(`./${fileName}.csv`,csv) //writing file with passed in file name
-    })
+        {fields: [
+            "property_id",
+            "account_number",
+            "name",
+            "address1",
+            "city",
+            "state_prov",
+            "postal_code",
+            "primary_contact_id"
+        ]
+        })
+        console.log(csv)
+        FileSystem.writeFileSync(`./${newFileName}.csv`,csv)
+    });
 }
 
-
-//Example:
-//converter(xmlFile,"tester")
+// Example:
+converter("Facility","NewFacility")
